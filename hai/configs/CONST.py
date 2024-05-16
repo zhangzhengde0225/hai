7b512d403cc21a6b08a00229a8bc85e3f77e6d88
zdzhang@ihep.ac.cn <zdzhang@ihep.ac.cn>
2024-05-16 10:34:53 +0800

    zzd update v1.1.9

--- hai/configs/CONST.py
+++ hai/configs/CONST.py
@@ -4,5 +4,5 @@ import os, sys
 from pathlib import Path    
 here = Path(__file__).parent
 
-ROOT_DIR = str(here.parent.parent)
+ROOT_DIR = here.parent.parent
 
--- hai/demos/request_via_client.py/rqst_client_image.py
+++ hai/demos/request_via_client.py/rqst_client_image.py
@@ -9,6 +9,7 @@ try:
 except:
     sys.path.insert(1, str(here.parent.parent.parent))
     from hepai import __version__
+from hai.configs import CONST
 
 from openai import OpenAI
 from hepai import HepAI, ChatCompletion, Stream
@@ -24,11 +25,11 @@ client = HepAI(
     proxy=proxy,
 )
 
-# client = OpenAI(
-#     api_key=os.getenv("OPENAI_API_KEY"),
-#     base_url=base_url,
-#     http_client=HepAI().get_http_client(proxy=proxy, base_url=base_url),
-# )
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"),
+    base_url=base_url,
+    http_client=HepAI().get_http_client(proxy=proxy, base_url=base_url),
+)
 
 completion: Union[ChatCompletion, Stream] = client.chat.completions.create(
   model="gpt-4o",
@@ -37,16 +38,19 @@ completion: Union[ChatCompletion, Stream] = client.chat.completions.create(
           "role": "user",
           "content": [
             {"type": "text", "text": "What's in this image?"},
-            # {
-            #   "type": "image_url",
-            #   "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
-            # },
+            {
+              "type": "image_url",
+              "image_url": {
+                  "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+                  "detail": "low"
+              }
+            },
           ],
         }
     ],
   max_tokens=300,
-  stream=True,
-  stream_options={"include_usage": True},
+  stream=False,
+  # stream_options={"include_usage": True},
 )
 
 if isinstance(completion, Stream):
