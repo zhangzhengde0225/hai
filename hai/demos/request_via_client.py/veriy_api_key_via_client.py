a3b03de0b6f8f1b33435d0a1e1ed4116cc71fd65
zdzhang@ihep.ac.cn <zdzhang@ihep.ac.cn>
2024-07-08 13:23:40 +0800

    zzd

--- hai/demos/request_via_client.py/veriy_api_key_via_client.py
+++ hai/demos/request_via_client.py/veriy_api_key_via_client.py
@@ -16,11 +16,7 @@ from hepai import HepAI, ChatCompletion, Stream
 # base_url = "http://localhost:42901/v1"
 base_url = "https://aiapi.ihep.ac.cn/v1"
 
-client = HepAI(
-    api_key=os.getenv("OPENAI_API_KEY"),
-    base_url=base_url,
-    max_retries=0,
-    )
+client = HepAI(api_key=os.getenv("HEPAI_API_KEY"), base_url=base_url, max_retries=0)
 
 api_key = os.getenv("HEPAI_API_KEY")
 rst: Dict = client.verify_api_key(api_key=api_key)
