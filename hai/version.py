ad77f93477bfd53873ba12b3ffd52805e3bc419c
zdzhang@ihep.ac.cn <zdzhang@ihep.ac.cn>
2025-06-16 14:19:28 +0800

    zzd

--- hai/version.py
+++ hai/version.py
@@ -1,5 +1,5 @@
 __appname__ = 'hepai'
-__version__ = '1.1.35'
+__version__ = '1.1.36'
 __version_suffix__ = 'Beta'  # version suffix, like: 1.0.0-Dev, Beta, Stable
 __author__ = 'ZhengDe Zhang'
 __email__ = 'zdzhang@ihep.ac.cn'
