30258f9e66a5fcf754f04c31ce41b1f70f2250a0
zdzhang@ihep.ac.cn <zdzhang@ihep.ac.cn>
2025-06-17 15:49:56 +0800

    zzd

--- hepai/components/hai_ecs/hai_ecs_v2.py
+++ hepai/components/hai_ecs/hai_ecs_v2.py
@@ -385,7 +385,7 @@ ECS information:
     User: {self.username}
     Port: {connection_info.gateway_port}
     
-    You can connect to it via: `ssh {self.username}@{connection_info.host} -p {connection_info.gateway_port}`""")
+    You can connect to it via: `ssh -o StrictHostKeyChecking=no {self.username}@{connection_info.host} -p {connection_info.gateway_port}`""")
             exit(0)  # 如果有正在运行的作业，则直接退出
         
         job_info = self.submit_enode_job()
@@ -403,7 +403,7 @@ The ECS is ready!
     User: {self.username}
     Port: {connection_info.gateway_port}
     
-    You can connect to it via: `ssh {self.username}@{connection_info.host} -p {connection_info.gateway_port}`
+    You can connect to it via: `ssh -o StrictHostKeyChecking=no {self.username}@{connection_info.host} -p {connection_info.gateway_port}`
     For more information, please visit: `https://ai.ihep.ac.cn/docs`
     """)
         return connection_info
