adb305caf86bd0b67ba29b5b4d21572e8946824e
zdzhang@ihep.ac.cn <zdzhang@ihep.ac.cn>
2025-09-12 12:14:56 +0800

    zzd

--- hepai/workers/ark/run_ark_worker.sh
+++ hepai/workers/ark/run_ark_worker.sh
@@ -5,7 +5,7 @@ python ark_worker.py \
     --port 0 \
     --controller_address https://aiapi.ihep.ac.cn \
     --no_register False \
-    --permissions "groups: payg, haichat, drsai, haioverleaf" \
+    --permissions "groups: payg, haichat, drsai, haioverleaf, haiacademic" \
     --debug True \
     $@
 
