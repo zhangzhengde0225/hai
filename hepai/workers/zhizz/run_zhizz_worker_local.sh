6cd381240129b4af780e7ad82ab09e632e214ae9
zdzhang@ihep.ac.cn <zdzhang@ihep.ac.cn>
2025-09-15 00:42:45 +0800

    zzd

--- hepai/workers/zhizz/run_zhizz_worker_local.sh
+++ hepai/workers/zhizz/run_zhizz_worker_local.sh
@@ -4,6 +4,6 @@ python zhizz_worker.py \
     --controller_address "http://localhost:42601" \
     --port 0 \
     --no_register False \
-    --permissions "groups: payg, haichat, drsai, haioverleaf, haiacademic;owner: zdzhang" \
-    --debug True \
+    --permissions "groups: payg, haichat, drsai, haioverleaf, haiacademic;owner: zdzhang@ihep.ac.cn" \
+    --debug False \
     $@
