29606a92c099daa62f912c64c87dd4938ad0c765
zdzhang@ihep.ac.cn <zdzhang@ihep.ac.cn>
2025-09-14 20:03:05 +0800

    zzd

--- hepai/workers/zhizz/run_zhizz_worker.sh
+++ hepai/workers/zhizz/run_zhizz_worker.sh
@@ -3,7 +3,7 @@
 python zhizz_worker.py \
     --controller_address "https://aiapi.ihep.ac.cn" \
     --no_register False \
-    --permissions "groups: payg, haichat, drsai, haioverleaf, haiacademic" \
+    --permissions "groups: payg, haichat, drsai, haioverleaf, haiacademic;owner: zdzhang@ihep.ac.cn" \
     --debug True \
     $@
 
