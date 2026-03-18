8e8cc9dce2c3b03e74e68ea6e17ad241f692a91c
zhangyiyu <zhangyiyu@example.com>
2026-03-18 12:09:07 +0800

    update test model code

--- hepai/workers/zhizz/run_zhizz_worker.sh
+++ hepai/workers/zhizz/run_zhizz_worker.sh
@@ -3,7 +3,7 @@
 python zhizz_worker.py \
     --controller_address "https://aiapi.ihep.ac.cn" \
     --no_register False \
-    --permissions "groups: payg, haichat, drsai, haioverleaf, haiacademic;owner: zdzhang@ihep.ac.cn" \
+    --permissions "groups: payg, haichat, drsai, haioverleaf, haiacademic, hairongzai@ihep.ac.cn, haik8s;owner: zdzhang@ihep.ac.cn" \
     --debug True \
     $@
 
--- hepai/workers/zhizz/run_zhizz_worker_local.sh
+++ hepai/workers/zhizz/run_zhizz_worker_local.sh
@@ -1,7 +1,7 @@
 
 
 python zhizz_worker.py \
-    --controller_address "http://localhost:42500" \
+    --controller_address "http://localhost:42601" \
     --port 42605 \
     --no_register False \
     --permissions "groups: payg, haichat, drsai, haioverleaf, haiacademic;owner: zdzhang@ihep.ac.cn" \
