0cd4c94937fd568b4b1598a3ebcbf89f3979fd38
zhangzhengde0225 <7729133+zzd0225@user.noreply.gitee.com>
2026-04-13 12:57:07 +0800

    zzd update v1.3.9

--- hai/version.py
+++ hai/version.py
@@ -1,5 +1,5 @@
 __appname__ = 'hepai'
-__version__ = '1.3.8'
+__version__ = '1.3.9'
 __version_suffix__ = 'Beta'  # version suffix, such as: 1.0.0-Dev, Beta, Stable
 __author__ = 'ZhengDe Zhang'
 __email__ = 'zdzhang@ihep.ac.cn, xionddb@ihep.ac.cn'
